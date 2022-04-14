3934df5ea8e6279968d7ed0f9a775e05f48af5fc
KingAusti <austin@thehenrys.xyz>
2022-04-13 20:50:35 -0700

    setting up catch all and initial db query

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 const { appendFile } = require('fs');
 
 const PORT = process.env.PORT || 3001;
@@ -15,12 +16,27 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mySQL un,
+        user: 'root',
+        //your mySQL pw,
+        password: 'divine-pop-grid-dory',
+        database: 'election'
+    },
+    console.log("Connected to the election database.")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 //default response for any other request(Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
