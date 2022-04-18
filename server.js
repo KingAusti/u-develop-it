b50efa12edc3ca965682393ba33db51dd0cb6369
KingAusti <austin@thehenrys.xyz>
2022-04-17 18:04:39 -0700

    finishing voter modularization

--- server.js
+++ server.js
@@ -1,31 +1,23 @@
 const express = require('express');
 const db = require('./db/connection');
-const { appendFile } = require('fs');
-const inputCheck = require('./utils/inputCheck');
 const apiRoutes = require('./routes/apiRoutes');
-router.use(require('./partyRoutes'));
-
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
-//express middleware
+// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Use apiRoutes
 app.use('/api', apiRoutes);
 
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-//   });
-
-//default response for any other request(Not Found)
+// Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
+// Start server after DB connection
 db.connect(err => {
   if (err) throw err;
   console.log('Database connected.');
@@ -33,7 +25,3 @@ db.connect(err => {
     console.log(`Server running on port ${PORT}`);
   });
 });
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
